73d19cef4d8853333f0ab2a6f54c5403a02263d8
thelsk <github@thels.nl>
2020-01-21 11:17:03 +0100

    Add three Amazon SAS servers to app/bundles/EmailBundle/Form/Type/ConfigType.php

--- app/bundles/EmailBundle/Form/Type/ConfigType.php
+++ app/bundles/EmailBundle/Form/Type/ConfigType.php
@@ -322,9 +322,12 @@ class ConfigType extends AbstractType
             'choice',
             [
                 'choices'     => [
-                    'email-smtp.eu-west-1.amazonaws.com' => 'mautic.email.config.mailer.amazon_host.eu_west_1',
-                    'email-smtp.us-east-1.amazonaws.com' => 'mautic.email.config.mailer.amazon_host.us_east_1',
-                    'email-smtp.us-west-2.amazonaws.com' => 'mautic.email.config.mailer.amazon_host.eu_west_2',
+                    'email-smtp.eu-west-1.amazonaws.com'      => 'mautic.email.config.mailer.amazon_host.eu_west_1',
+                    'email-smtp.us-east-1.amazonaws.com'      => 'mautic.email.config.mailer.amazon_host.us_east_1',
+                    'email-smtp.us-west-2.amazonaws.com'      => 'mautic.email.config.mailer.amazon_host.eu_west_2',
+                    'email-smtp.eu-central-1.amazonaws.com'   => 'mautic.email.config.mailer.amazon_host.eu_central_1',
+                    'email-smtp.ap-south-1.amazonaws.com'     => 'mautic.email.config.mailer.amazon_host.ap_south_1',
+                    'email-smtp.ap-southeast-2.amazonaws.com' => 'mautic.email.config.mailer.amazon_host.ap_southeast_2',
                 ],
                 'label'       => 'mautic.email.config.mailer.amazon_host',
                 'required'    => false,
